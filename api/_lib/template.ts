3556a1fc60cc92e49d7831dae269aba1aba4f6bd
saml33 <slam.uke@gmail.com>
2021-10-22 12:49:46 +1100

    test-template

--- api/_lib/template.ts
+++ api/_lib/template.ts
@@ -1,62 +1,22 @@
+import marked from "marked";
+import { sanitizeHtml } from "./sanitizer";
+import { ParsedRequest } from "./types";
 
-import { readFileSync } from 'fs';
-import marked from 'marked';
-import { sanitizeHtml } from './sanitizer';
-import { ParsedRequest } from './types';
-const twemoji = require('twemoji');
-const twOptions = { folder: 'svg', ext: '.svg' };
-const emojify = (text: string) => twemoji.parse(text, twOptions);
-
-const rglr = readFileSync(`${__dirname}/../_fonts/Inter-Regular.woff2`).toString('base64');
-const bold = readFileSync(`${__dirname}/../_fonts/Inter-Bold.woff2`).toString('base64');
-const mono = readFileSync(`${__dirname}/../_fonts/Vera-Mono.woff2`).toString('base64');
-
-function getCss(theme: string, fontSize: string) {
-    let background = 'white';
-    let foreground = 'black';
-    let radial = 'lightgray';
-
-    if (theme === 'dark') {
-        background = 'black';
-        foreground = 'white';
-        radial = 'dimgray';
-    }
-    return `
-    @font-face {
-        font-family: 'Inter';
-        font-style:  normal;
-        font-weight: normal;
-        src: url(data:font/woff2;charset=utf-8;base64,${rglr}) format('woff2');
-    }
-
-    @font-face {
-        font-family: 'Inter';
-        font-style:  normal;
-        font-weight: bold;
-        src: url(data:font/woff2;charset=utf-8;base64,${bold}) format('woff2');
-    }
-
-    @font-face {
-        font-family: 'Vera';
-        font-style: normal;
-        font-weight: normal;
-        src: url(data:font/woff2;charset=utf-8;base64,${mono})  format("woff2");
-      }
-
+function getCss(side: string, pnl: string) {
+  const pnlColor = parseFloat(pnl) >= 0 ? "#AFD803" : "#E54033";
+  const sideColor = side.toLowerCase() === "long" ? "#AFD803" : "#E54033";
+  return `
     body {
-        background: ${background};
-        background-image: radial-gradient(circle at 25px 25px, ${radial} 2%, transparent 0%), radial-gradient(circle at 75px 75px, ${radial} 2%, transparent 0%);
+        background: #141026;
         background-size: 100px 100px;
+        color: white;
+        font-family: 'Lato', sans-serif;
         height: 100vh;
-        display: flex;
-        text-align: center;
-        align-items: center;
-        justify-content: center;
+        padding: 120px 256px;
     }
 
     code {
         color: #D400FF;
-        font-family: 'Vera';
         white-space: pre-wrap;
         letter-spacing: -5px;
     }
@@ -87,60 +47,142 @@ function getCss(theme: string, fontSize: string) {
         margin: 150px;
     }
 
-    .emoji {
-        height: 1em;
-        width: 1em;
-        margin: 0 .05em 0 .1em;
-        vertical-align: -0.1em;
+    .side-market {
+        display: flex;
+        font-size: 40px;
+        align-items: center;
+        justify-content: center;
+        padding: 40px 40px 24px;
+    }
+
+    .side {
+        color: ${sideColor};
+        padding: 0px 24px;
+    }
+
+    .divider {
+        color: rgba(255,255,255,0.5);
+        margin-bottom: 8px;
+    }
+
+    .market {
+        padding: 0px 24px;
+    }
+
+    .pnl {
+        border: 4px ${pnlColor} solid;
+        border-radius: 24px;
+        color: ${pnlColor};
+        display: flex;
+        font-size: 140px;
+        font-weight: bold;
+        height: 240px;
+        align-items: center;
+        justify-content: center;
     }
     
-    .heading {
-        font-family: 'Inter', sans-serif;
-        font-size: ${sanitizeHtml(fontSize)};
-        font-style: normal;
-        color: ${foreground};
-        line-height: 1.8;
+    .trade-details-wrapper {
+        display: flex;
+        justify-content: space-between;
+        padding: 0px 200px;
+    }
+
+    .trade-details {
+        font-size: 56px;
+        font-weight: bold;
+        line-height: 0.5;
+        padding: 100px;
+    }
+    
+    .label {
+        color: rgba(255,255,255,0.5);
+        font-size: 40px;
+        font-weight: normal;
     }`;
 }
 
 export function getHtml(parsedReq: ParsedRequest) {
-    const { text, theme, md, fontSize, images, widths, heights } = parsedReq;
-    return `<!DOCTYPE html>
+  const {
+    market,
+    side,
+    pnl,
+    avgEntry,
+    markPrice,
+    md,
+    images,
+    widths,
+    heights,
+  } = parsedReq;
+  return `<!DOCTYPE html>
 <html>
     <meta charset="utf-8">
     <title>Generated Image</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;900&display=swap" rel="stylesheet">
     <style>
-        ${getCss(theme, fontSize)}
+        ${getCss(side, pnl)}
     </style>
     <body>
         <div>
-            <div class="spacer">
             <div class="logo-wrapper">
-                ${images.map((img, i) =>
-                    getPlusSign(i) + getImage(img, widths[i], heights[i])
-                ).join('')}
+                ${images
+                  .map(
+                    (img, i) =>
+                      getPlusSign(i) + getImage(img, widths[i], heights[i])
+                  )
+                  .join("")}
             </div>
-            <div class="spacer">
-            <div class="heading">${emojify(
-                md ? marked(text) : sanitizeHtml(text)
-            )}
+            <div class="side-market"><span class="side">${
+              md ? marked(side.toUpperCase()) : sanitizeHtml(side.toUpperCase())
+            }</span>
+            <span class="divider">|</span>
+            <span class="market">${
+              md ? marked(market) : sanitizeHtml(market)
+            }</span>
+            </div>
+            <div class="pnl">${md ? marked(`${pnl}%`) : sanitizeHtml(`${pnl}%`)}
+            </div>
+            <div class="trade-details-wrapper">
+                <div class="trade-details">
+                    <div class="label">Avg Entry Price</div>
+                    <div>${
+                      md
+                        ? marked(`$${parseFloat(avgEntry).toLocaleString()}`)
+                        : sanitizeHtml(
+                            `$${parseFloat(avgEntry).toLocaleString()}`
+                          )
+                    }
+                    </div>
+                </div>
+                <div class="trade-details">
+                    <div class="label">Mark Price</div>
+                    <div>${
+                      md
+                        ? marked(`$${parseFloat(markPrice).toLocaleString()}`)
+                        : sanitizeHtml(
+                            `$${parseFloat(markPrice).toLocaleString()}`
+                          )
+                    }
+                    </div>
+                </div>
             </div>
         </div>
     </body>
 </html>`;
 }
 
-function getImage(src: string, width ='auto', height = '225') {
-    return `<img
+function getImage(src: string, width = "auto", height = "204") {
+  return `<img
         class="logo"
         alt="Generated Image"
         src="${sanitizeHtml(src)}"
         width="${sanitizeHtml(width)}"
         height="${sanitizeHtml(height)}"
-    />`
+    />`;
 }
 
 function getPlusSign(i: number) {
-    return i === 0 ? '' : '<div class="plus">+</div>';
+  return i === 0 ? "" : '<div class="plus">+</div>';
 }
